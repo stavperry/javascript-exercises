6823235e2c8fc612265b8475c45c940b38061f4b
Stav <162802041+stavperry@users.noreply.github.com>
2025-04-08 11:42:38 +0100

    JS first 3 exercises completed and tested

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+function repeatString(word, number) {
+    if (number < 0) {
+        return "ERROR";
+    } else {
+        let result = '';
+        for (let i = 0; i < number; i++) {
+            result += word;
+        }
+        return result;
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+    let phraseArr = [];
+    for (letter of phrase) {
+        phraseArr.push(letter); 
+    } // so now we'll have phraseArr = ['h', 'e', 'l', 'l', 'o']
+    let newArr = [];
+    for (let i = phraseArr.length - 1; i >= 0 ; i--) {
+        newArr.push(phraseArr[i]);
+    }
+    return newArr.join('');
 };
 
 // Do not edit below this line
