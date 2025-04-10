b255348d47c1d2b5ebe404bc6597d0621e121bd4
Stav <162802041+stavperry@users.noreply.github.com>
2025-04-10 18:46:56 +0100

    leap year solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    // if divisible by 4 - might be leap year. if not - not a leap year.
+    // if it's divisible by 4, it needs to not be divisible by 100. if it is,
+    // it needs to be divisible by 400.
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
