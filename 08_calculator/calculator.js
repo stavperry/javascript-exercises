110b0e25cff24544eb1aa8a6887308adf3854879
Stav <162802041+stavperry@users.noreply.github.com>
2025-04-11 21:41:11 +0100

    calculator exercise solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,79 @@
-const add = function() {
+const add = function(...x) {
+  if (x.length === 1) {
+    return x[0] + x[0];
+  } else {
+    let sum = 0;
+    for (let i = 0; i < x.length; i++) {
+      sum += x[i];
+    }
+    return sum;
+  }
+};
+
+const subtract = function(...x) {
+  if (x.length === 1) {
+    return 0;
+  } else {
+    let result = x[0];
+    for (let i = 1; i < x.length; i++) {
+      result -= x[i];
+    }
+    return result;
+  }
+};
+
+const sum = function(...x) {
+  if (x.length === 1 && Array.isArray(x[0])) {
+    x = x[0];
+  }
+  if (x.length === 0) {
+    return 0;
+  } else {  
+    let total = 0;
+    for (let i = 0; i < x.length; i++) {
+      total += Number(x[i]);
+    }
+    return total;
+  }
+};
+
+
+const multiply = function(...x) {
+  if (x.length === 1 && Array.isArray(x[0])) {
+    x = x[0];
+  }
+  if (x.length === 0) {
+    return 0;
+  } else if (x.length === 1) {
+    return Number(x[0]) * Number(x[0]);
+  } else {  
+    let result = 1;
+    for (let i = 0; i < x.length; i++) {
+      result *= Number(x[i]);
+    }
+    return result;
+  }
+};
+
+const power = function(...x) {
+  if (x.length === 1) {
+    return x[0] ** x[0];
+  } else {
+    return x[0] ** x[1];
+  }
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x === 0) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let i = 1; i <=x; i++) {
+      result *= i;
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
