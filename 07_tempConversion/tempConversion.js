5f69cbd5de8b13cbf13969a64d28b8801abe2d3b
Stav <162802041+stavperry@users.noreply.github.com>
2025-04-11 20:57:39 +0100

    Exercise 07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let newDeg = ((degree - 32) * (5/9)).toFixed(1);
+  newDeg = parseFloat(newDeg);
+  if (newDeg !== Math.floor(newDeg)) {
+    return newDeg;
+  } else {
+    return Math.floor(newDeg);
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let newDeg = ((degree * (9/5)) + 32);
+  if (newDeg !== Math.floor(newDeg)) {
+    return parseFloat(newDeg.toFixed(1));
+  } else {
+    return Math.floor(newDeg);
+  }
 };
 
 // Do not edit below this line
